b2a95e381591d3360757715a00ca628d9dccfa44
Barri3 <167632850+barri3@users.noreply.github.com>
2024-05-11 17:42:09 +0100

    Server logic (Part 4)

--- src/org/barri3/missionlog/SourceRemote.java
+++ src/org/barri3/missionlog/SourceRemote.java
@@ -4,13 +4,53 @@
  */
 package org.barri3.missionlog;
 
+import java.net.Socket;
+import org.barri3.missionlog.server.Connection;
+
 /**
  *
  * @author GB-BARRI3
  */
 public class SourceRemote extends AbstractSource {
+    private Connection conn = null;
+    private String serverAddress = null;
+    private int serverPort = -1;
+    private String serverPassword = null;
+    
     @Override
     public void run() {
         MissionLog.out.appendSystemInfo("remote");
+        
+        getServerAddressAndPort();
+        getServerPassword();
+        
+        connect();
+        doHandshake();
+        runLoop();
+        dispose();
+    }
+    
+    private void getServerAddressAndPort() {
+        //use settings as default value for fields
+    }
+    
+    private void getServerPassword() {
+        // dont use settings
+    }
+    
+    private void connect() {
+        
+    }
+    
+    private void doHandshake() {
+        
+    }
+    
+    private void runLoop() {
+        
+    }
+    
+    private void dispose() {
+        
     }
 }
---
+++ src/org/barri3/missionlog/server/Connection.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.barri3.missionlog.server;
+
+import java.net.Socket;
+
+/**
+ *
+ * @author GB-BARRI3
+ */
+public class Connection {
+    public Connection(Socket socket) {
+        
+    }
+    
+    public void send(BinaryMessage message) {
+        
+    }
+    
+    public void send(StringMessage message) {
+        
+    }
+    
+    public BinaryMessage readBinary() {
+        
+    }
+    
+    public StringMessage readString() {
+        
+    }
+}
