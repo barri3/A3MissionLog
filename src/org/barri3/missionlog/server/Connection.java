5f64c834b2b8933d057edf39d0f06fcbcba91244
barri3 <167632850+barri3@users.noreply.github.com>
2024-05-12 12:58:18 +0100

    Server logic (Part 5)
    Connection code

--- src/org/barri3/missionlog/server/Connection.java
+++ src/org/barri3/missionlog/server/Connection.java
@@ -4,30 +4,51 @@
  */
 package org.barri3.missionlog.server;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
+import uk.co.gamebrewers.bookworm.CBookwormBinarizer;
 
 /**
  *
  * @author GB-BARRI3
  */
 public class Connection {
-    public Connection(Socket socket) {
-        
+    private Socket socket = null;
+    private InputStream in = null;
+    private OutputStream out = null;
+    
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        this.in = socket.getInputStream();
+        this.out = socket.getOutputStream();
+    }
+
+    public void send(BinaryMessage message) throws IOException {
+        send(MessagePacker.pack(message));
     }
     
-    public void send(BinaryMessage message) {
-        
+    public void send(StringMessage message) throws IOException {
+        send(MessagePacker.pack(message));
     }
     
-    public void send(StringMessage message) {
-        
+    public void send(byte[] data) throws IOException {
+        out.write(data);
+        out.flush();
     }
     
-    public BinaryMessage readBinary() {
-        
+    public BinaryMessage readBinary() throws IOException {
+        return MessagePacker.unpackBinary(read());
     }
     
-    public StringMessage readString() {
-        
+    public StringMessage readString() throws IOException {
+        return MessagePacker.unpackString(read());
+    }
+    
+    public byte[] read() throws IOException {
+        byte[] lenBytes = in.readNBytes(4);
+        int length = CBookwormBinarizer.intFromBytes(lenBytes);
+        return in.readNBytes(length);
     }
 }
